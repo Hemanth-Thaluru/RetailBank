110e9a94b3b651a336a588767a45d4901a17f373
Hemanth <thaluruhemanth105@gmail.com>
2022-03-02 11:04:39 +0530

    Add project files.

---
+++ RetailBank UI/Controllers/LoginController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RetailBank_UI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RetailBank_UI.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly ILogger<LoginController> _logger;
+
+        public LoginController(ILogger<LoginController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Login()
+        {
+            
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(UserCred u)
+        {
+
+            var v = u.UserId;
+
+
+            return View();
+        }
+    }
+}
---
+++ RetailBank UI/Models/UserCred.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RetailBank_UI.Models
+{
+    public class UserCred
+    {
+        public int UserId { get; set; }
+        public string Password { get; set; }
+    }
+}
